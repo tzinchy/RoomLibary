d654dfd0bbbfeca3b9f28cbaaf722b632f394250
student <student@PC-VOL-231-07>
2023-04-12 09:31:48 +0300

    Добавьте файлы проекта.

---
+++ RoomLibary/Room.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomLibary
+{
+    public class Room
+    {
+        double roomLength;
+        double roomWidth;
+
+        public double RoomLength
+        {
+            get { return roomLength; }
+            set { roomLength = value; }
+        }
+        public double RoomWidth
+        {
+            get { return roomWidth; }
+            set { roomWidth = value; }
+        }
+        public double RoomPerimetr()
+        {
+            return 2 * (roomLength + roomWidth);
+        }
+        public double RoomArea()
+        {
+            return roomLength * roomWidth;
+        }
+        public double PersonArea(int np)
+        {
+            return RoomArea() / np;
+        }
+    }
+}
